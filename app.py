a77a8df2c56273fcb6a3439afb2a140d4df88203
HaoYunLianLian <innoai@users.noreply.huggingface.co>
2024-12-30 03:24:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -77,4 +77,5 @@ async def create_demo():
 # Run the application
 if __name__ == "__main__":
     demo = asyncio.run(create_demo())
+    demo.queue(concurrency_count=5, max_size=None, timeout=None)
     demo.launch(show_api=False)
